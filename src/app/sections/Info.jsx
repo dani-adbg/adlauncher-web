d9581677b84f65679db64a48bed684bf6375d082
Alexander Barreto <adguerrero59@gmail.com>
2024-03-09 15:30:01 -0500

    info style

--- src/app/sections/Info.jsx
+++ src/app/sections/Info.jsx
@@ -1,12 +1,13 @@
 export default function Info() {
   return (
-    <div className="text-center my-[10vh]">
-      <span>¿Qué es ADLauncher?</span>
-      <p>ADLauncher es un lanzador de minecraft no premium de código abierto creado con adlauncher-core y electron.</p>
-      <p>El objetivo de ADLauncher es ser una opción para que puedas jugar minecraft sin problemas hasta que lo puedas comprar.</p>
-      <p>ADLauncher soporta minecraft Vanilla, Forge, Optifine y Fabric para que puedas jugar sin complicaciones.</p>
-      <p>No apoyamos a la piratería. Recuerda comprar minecraft cuando puedas.</p>
-      <p>Mientras disfruta el launcher :)</p>
+    <div className="text-center py-[10vh] bg-gray">
+      <span className="text-xl">¿Qué es ADLauncher?</span>
+      <p className="my-1">ADLauncher es un lanzador de minecraft no premium de código abierto creado con adlauncher-core y electron.</p>
+      <p className="my-1">El objetivo de ADLauncher es ser una opción para que puedas jugar minecraft sin problemas hasta que lo puedas comprar.</p>
+      <p className="my-1">ADLauncher soporta minecraft Vanilla, Forge, Optifine y Fabric para que puedas jugar sin complicaciones.</p>
+      <p className="my-1">No apoyamos a la piratería. Recuerda comprar minecraft cuando puedas.</p>
+      <p className="my-1">Mientras disfruta el launcher :)</p>
+      <p className="my-1">Se nos acabó el presupuesto para la página.</p>
     </div>
   )
 }
