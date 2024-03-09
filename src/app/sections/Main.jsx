62383754c6755801149679e8017663aa5ad9d238
Alexander Barreto <adguerrero59@gmail.com>
2024-03-09 15:26:16 -0500

    main style

--- src/app/sections/Main.jsx
+++ src/app/sections/Main.jsx
@@ -10,10 +10,12 @@ export default function Main() {
         height={864}
         className="w-full h-[80vh] absolute top-[8vh] -z-50"
       />
-      <div className="flex flex-col text-white">
-        <span>ADLauncher</span>
-        <span>Juega Minecraft Gratis Sin Virus</span>
-        <a href="https://github.com/dani-adbg/adlauncher/releases/latest" target="_blank">Descarga para Windows</a>
+      <div className="flex w-full h-[30vh] justify-around text-center items-center">
+        <div className="h-full flex justify-center items-center flex-col">
+          <p className="my-[2vh] text-black font-extrabold text-2xl">ADLauncher</p>
+          <p className="my-[2vh] text-black font-extrabold text-2xl">Juega Minecraft Gratis Sin Virus</p>
+        </div>
+        <a href="https://github.com/dani-adbg/adlauncher/releases/latest" target="_blank" className="bg-gray h-[10vh] w-[40vh] rounded-full flex justify-center items-center hover:bg-black hover:cursor-pointer transition ease-linear delay-200">Descarga para Windows</a>
       </div>
     </main>
   )
