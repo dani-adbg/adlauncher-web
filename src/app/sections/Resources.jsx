6f6a9d42999796155c5ea9dd082acd55b1f6bbc2
Alexander Barreto <adguerrero59@gmail.com>
2024-03-09 16:24:17 -0500

    resources style

--- src/app/sections/Resources.jsx
+++ src/app/sections/Resources.jsx
@@ -1,11 +1,12 @@
 export default function Resources() {
   return (
-    <div id="resources">
-      <span>DESCARGAS</span>
-      <span>Descarga ADLauncher para Windows</span>
-      <span>Descarga el Instalador</span>
-      <span>Descarga el .zip (Recomendado)</span>
-      <span>Descarga la version sin interfaz</span>
+    <div id="downloads" className="bg-gray flex flex-col  items-center">
+      <span className="text-xl my-[2vh]">DESCARGAS</span>
+      <div className="flex flex-col justify-center my-[2vh] bg-black w-[70vh] h-[30vh] px-[10vh] rounded-xl">
+        <a href="https://github.com/dani-adbg/adlauncher/releases/download/1.0.0/adlauncher-1.0.0.Setup.exe" className="my-2 bg-gray rounded-lg h-[5vh] flex items-center px-[2vh] hover:bg-black cursor-pointer transition ease-linear delay-200">Descarga el Instalador</a>
+        <a href="https://github.com/dani-adbg/adlauncher/releases/download/1.0.0/adlauncher-1.0.0.zip" className="my-2 bg-gray rounded-lg h-[5vh] flex items-center px-[2vh] hover:bg-black cursor-pointer transition ease-linear delay-200">Descarga el .zip (Recomendado)</a>
+        <a href="https://github.com/dani-adbg/adlauncher/releases/download/1.0.0/adlauncher-1.0.0-terminal.exe" className="my-2 bg-gray rounded-lg h-[5vh] flex items-center px-[2vh] hover:bg-black cursor-pointer transition ease-linear delay-200">Descarga la version sin interfaz</a>
+      </div>
     </div>
   )
 }
