030ab5efb0215f67747a9082db13b523f5ccaf49
Alexander Barreto <adguerrero59@gmail.com>
2024-04-12 09:27:32 -0500

    adlauncher - v2.0.1

--- src/app/sections/Resources.jsx
+++ src/app/sections/Resources.jsx
@@ -3,7 +3,7 @@ export default function Resources() {
     <div id="downloads" className="bg-gray flex flex-col items-center pb-[6vh]">
       <span className="text-xl font-bold mb-[2vh]">DESCARGAS</span>
       <div className="flex flex-col justify-center my-[2vh] bg-black w-[70vh] h-[30vh] px-[10vh] rounded-xl">
-        <a href="https://github.com/dani-adbg/adlauncher/releases/download/2.0.0/adlauncher-v2.0.zip" className="my-2 bg-gray rounded-lg h-[5vh] flex items-center px-[2vh] hover:bg-black cursor-pointer transition ease-linear delay-200">Descarga el .zip (Recomendado)</a>
+        <a href="https://github.com/dani-adbg/adlauncher/releases/download/2.0.1/adlauncher-win32-x64-v2.0.1.zip" className="my-2 bg-gray rounded-lg h-[5vh] flex items-center px-[2vh] hover:bg-black cursor-pointer transition ease-linear delay-200">Descarga el .zip (Recomendado)</a>
         <a href="https://github.com/dani-adbg/adlauncher/releases/download/1.0.0/adlauncher-1.0.0-terminal.exe" className="my-2 bg-gray rounded-lg h-[5vh] flex items-center px-[2vh] hover:bg-black cursor-pointer transition ease-linear delay-200">Descarga la version sin interfaz</a>
       </div>
     </div>
